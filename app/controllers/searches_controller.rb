fbf03457a640dce7924624387e64d104c9e29c1f
y.sasamori <info.yuu09@gmail.com>
2022-01-29 08:11:43 +0000

    CRUD searches

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -9,17 +9,49 @@ class SearchesController < ApplicationController
   end
 
   def new
+    @search = Search.new
   end
 
   def create
+    @search = Search.new(search_params)
+    
+    if @search.save 
+      flash[:success] = '投稿されました。'
+      redirect_to @search
+    else
+      flash.now[:danger] = '投稿に失敗しました。'
+      render :new
+    end
   end
 
   def edit
+    @search = Search.find(params[:id])
   end
 
   def update
+    @search = Search.find(params[:id])
+    
+    if @search.update(search_params)
+      flash[:success] = '更新されました。'
+      redirect_to @search
+    else
+      flash.now[:danger] = '更新に失敗しました。'
+      render :edit
+    end
   end
 
   def destroy
+    @search = Search.find(params[:id])
+    @search.destroy
+    
+    flash[:success] = '削除されました。'
+    redirect_to searches_url
   end
+  
+  private
+  
+    def search_params
+      params.require(:search).permit(:condition_name)
+      params.require(:search).permit(:content)
+    end
 end
