331e8fef398ddfa17fee831deb1871559005f8b6
y.sasamori <info.yuu09@gmail.com>
2022-02-02 11:33:26 +0000

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'users/new'
+  get 'users/create'
     root to: 'searches#index'
 
+    get 'signup', to: 'users#new'
+    resources :users, only: [:index, :show, :create]
     resources :searches
 end
