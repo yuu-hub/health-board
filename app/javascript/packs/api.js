95fb681c69d144f83ffae187e6d65e9413f83f10
y.sasamori <info.yuu09@gmail.com>
2022-03-12 08:53:42 +0000

    commentout api

--- app/javascript/packs/api.js
+++ app/javascript/packs/api.js
@@ -1,21 +1,21 @@
-// リクエストパラメータを作る
-const apiKey = 'AIzaSyAJCDSQx3tpg2O0Axt2PrKwH9oYfcpm_Rc';
-const cx = 'fee06ffd8f22fe8d1';
-const keyword = '鉄分 レシピ';
-const url = `https://www.googleapis.com/customsearch/v1?key=${apiKey}&cx=${cx}&q=${keyword}`;
-console.log(url);
+// // リクエストパラメータを作る
+// const apiKey = 'AIzaSyAJCDSQx3tpg2O0Axt2PrKwH9oYfcpm_Rc';
+// const cx = 'fee06ffd8f22fe8d1';
+// const keyword = '鉄分 レシピ';
+// const url = `https://www.googleapis.com/customsearch/v1?key=${apiKey}&cx=${cx}&q=${keyword}`;
+// console.log(url);
 
-const app = new Vue({
-    el: '#recipe',
-    data: {
-      items: [],
-    },
-    created() {
-      fetch(url)
-      .then((response) => response.json())
-      .then((data) =>{
-        this.items = data.items;
+// const app = new Vue({
+//     el: '#recipe',
+//     data: {
+//       items: [],
+//     },
+//     created() {
+//       fetch(url)
+//       .then((response) => response.json())
+//       .then((data) =>{
+//         this.items = data.items;
         
-      });
-    }
-  })
+//       });
+//     }
+//   })
